bfdd1965d945fce697c927682cd6d9cbd2001ae2
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-07-24 21:49:02 -0700

    Updated version

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  console.log('hello');
+  $('#chandelier').addClass('animated slideInDown');
+  //$('#right').addClass('animated bounceInRight');
+  $('#titleName').addClass('animated bounceInUp')
+
+})
